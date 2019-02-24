2186f216116a0aceb9b50b2d459a261587ea36e1
justkevinQA <kevin.just@qaware.de>
2019-02-24 13:39:50 +0100

    added the simple publisher and an according launch file

---
+++ scripts/simple_publisher.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# license removed for brevity
+
+import rospy
+from std_msgs.msg import String
+from bondpy import bondpy
+
+bond = bondpy.Bond("skill_machine_bonds", "NODE_NAME_bond")
+bond.start()
+
+
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10)  # 10hz
+    while not rospy.is_shutdown():
+        hello_str = "hello world %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass

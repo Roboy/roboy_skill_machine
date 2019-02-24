93f5f296e4f681a806435a0ae6de88b1b7562ab7
justkevinQA <kevin.just@qaware.de>
2019-02-24 18:40:24 +0100

    seemed to not work, new approach

--- scripts/start_simple_publisher.sh
+++ scripts/start_simple_publisher.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
 
-skill_data=("")
-if [ $1 = "simple_publisher" ]; then
-    skill_data = "skill_name: 'simple_publisher'
+source ~/catkin_ws/devel/setup.bash
+
+if [[ $1 = "simple_publisher" ]];  then
+    rosservice call /start_skill "skill_name: 'simple_publisher'
     launch_package: 'roboy_skill_machine'
     launch_file: 'simple_pub.launch'
     continuous: true
     node_list:
     - {node_name: 'simple_publisher', node_executable: 'simple_publisher.py', node_package: 'roboy_skill_machine'}"
 fi
-
-
-source ~/catkin_ws/devel/setup.bash
-rosservice call /start_skill ${skill_data}
 

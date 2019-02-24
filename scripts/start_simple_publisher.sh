97b904d995d96b44c36e9a4da9c4b5bc4fd50124
justkevinQA <kevin.just@qaware.de>
2019-02-24 18:33:14 +0100

    changed the call again a bit

--- scripts/start_simple_publisher.sh
+++ scripts/start_simple_publisher.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-skill_data = "skill_name: 'simple_publisher'
-launch_package: 'roboy_skill_machine'
-launch_file: 'simple_pub.launch'
-continuous: true
-node_list:
-- {node_name: 'simple_publisher', node_executable: 'simple_publisher.py', node_package: 'roboy_skill_machine'}"
+
+skill_data=("")
+if [ $1 = "simple_publisher" ]; then
+    skill_data = "skill_name: 'simple_publisher'
+    launch_package: 'roboy_skill_machine'
+    launch_file: 'simple_pub.launch'
+    continuous: true
+    node_list:
+    - {node_name: 'simple_publisher', node_executable: 'simple_publisher.py', node_package: 'roboy_skill_machine'}"
+fi
 
 
 source ~/catkin_ws/devel/setup.bash

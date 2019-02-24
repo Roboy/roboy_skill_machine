de649f1a94060176fcb2e9608b631388b886327f
justkevinQA <kevin.just@qaware.de>
2019-02-24 18:27:32 +0100

    variablized the call

--- scripts/start_simple_publisher.sh
+++ scripts/start_simple_publisher.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-source ~/catkin_ws/devel/setup.bash
-rosservice call /start_skill "skill_name: 'simple_publisher'
+skill_data = "skill_name: 'simple_publisher'
 launch_package: 'roboy_skill_machine'
 launch_file: 'simple_pub.launch'
 continuous: true
 node_list:
 - {node_name: 'simple_publisher', node_executable: 'simple_publisher.py', node_package: 'roboy_skill_machine'}"
+
+
+source ~/catkin_ws/devel/setup.bash
+rosservice call /start_skill ${skill_data}
 

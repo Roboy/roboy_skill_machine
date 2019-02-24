bda541f69c935d4b258ba96160b55b9b577d03e2
justkevinQA <kevin.just@qaware.de>
2019-02-24 15:25:08 +0100

    removed the loginfo, because it disturbs the devel process

--- scripts/simple_publisher.py
+++ scripts/simple_publisher.py
@@ -16,7 +16,7 @@ def talker():
     rate = rospy.Rate(10)  # 10hz
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
+        # rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
 

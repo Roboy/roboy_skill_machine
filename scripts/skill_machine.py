0481e0a728acd1c1d4f909323041237f135bd4b5
rynoci <rjnovak14@gmail.com>
2019-03-05 10:05:38 +0100

    First day of hackathon

--- scripts/skill_machine.py
+++ scripts/skill_machine.py
@@ -7,11 +7,14 @@ from bondpy import bondpy
 import std_msgs.msg
 import os
 import subprocess
-import roboy_cognition_msgs.msg
+#import roboy_cognition_msgs.msg
 from roboy_skill_machine.msg import SkillNode
-from roboy_skill_machine.srv import StartSkill
+from roboy_skill_machine.srv import LaunchSkill
+from roboy_skill_machine.srv import ExecuteSkill
+from roboy_skill_machine.srv import TerminateSkill
+from roboy_communication_cognition.msg import *
 
-
+# Class to hold all relevant data for skills
 class Skill:
     def __init__(self, skill_name, launch_package, launch_file, continuous, node_list):
         self.skill_name = skill_name
@@ -21,12 +24,13 @@ class Skill:
         self.node_name_list = []
         self.add_nodes(node_list)
 
+    # Pulls all nodes provided by argument and attaches them to skill
     def add_nodes(self, node_list):
         for node in node_list:
-            #TODO check if this works
+#TODO check if this works
             if node.node_name not in node_dict:
                 bond = create_bond(node.node_name + "_bond")
-                new_node = Node(node.node_name, node.node_executable, node.node_package, self.continuous, bond)
+                new_node = Node(node.node_name, node.node_executable, node.node_package, node.node_machine, self.continuous, bond)
                 node_dict[node.node_name] = new_node
             node_dict[node.node_name].skill_name_list.append(self.skill_name)
             #Ensures that if skill is continuous, node is also continuous, but also that skill doesn't 
@@ -38,12 +42,13 @@ class Skill:
         self.node_name_list = list(set(self.node_name_list))
 
 
-
+# Class to hold all relevant data for nodes
 class Node:
-    def __init__(self, node_name, node_executable, node_package, continuous, bond):
+    def __init__(self, node_name, node_executable, node_package, node_machine, continuous, bond):
         self.node_name = node_name
         self.node_executable = node_executable
         self.node_package = node_package
+        self.node_machine = node_machine
         self.continuous = continuous
         self.bond = bond
         self.skill_name_list = []
@@ -55,14 +60,16 @@ class Node:
         return self.__dict__ == other.__dict__
 
 
-
-def handle_start_skill(request):
+# Runs the method to actually launch the skill, creates a new skill object for that skill, and adds it
+# to the list of skills maintained by the skill machine
+def handle_launch_skill(request):
     launch_skill(request.launch_package, request.launch_file)
     new_skill = Skill(request.skill_name, request.launch_package, request.launch_file, request.continuous, request.node_list)
     skill_dict[request.skill_name] = new_skill
     return 1
 
 
+# Actually launches the skill by running the roslaunch command on the provided launch file
 def launch_skill(package, launch_file):
     command = "roslaunch {0} {1}".format(package, launch_file)
     p = subprocess.Popen(command, shell=True)
@@ -77,6 +84,30 @@ def launch_skill(package, launch_file):
     return True
 
 
+#TODO Test
+def handle_execute_skill(request):
+    execute_skill(request.command)
+    new_skill = Skill(request.skill_name, None, request.command, request.continuous, request.node_list)
+    skill_dict[request.skill_name] = new_skill
+    return 1
+
+
+def execute_skill(command):
+    p = subprocess.Popen(command, shell=True)
+    state = p.poll()
+    if state is None:
+        rospy.loginfo("process is running fine")
+    elif state < 0:
+        rospy.loginfo("Process terminated with error")
+        return False
+    elif state > 0:
+        rospy.loginfo("Process terminated without error")
+    return True
+
+
+# Creates a bond between the skill machine and each node launched by the skill machine,
+# so that the skill machine is aware if any node dies. From there, skill machine can attempt
+# to restart the node or alert the user.
 def create_bond(node_id):
     bond = bondpy.Bond("skill_machine_bonds", node_id)
     bond.start()
@@ -85,13 +116,17 @@ def create_bond(node_id):
     return bond
 
 
+# Method called to check on node status. Iterates through nodes to see which ones are no longer
+# running. If they should be continuous, it attempts to restart them. If they should not be,
+# it removes them from the skill machine, and from any skill object holding them. It then goes
+# through the skills in the skill machine. Any skill that has no nodes running is removed from
+# the skill machine.
 def check_nodes_still_running():
     items_to_remove = []
     for node_name in node_dict:
         if (node_dict[node_name].bond.is_broken() and node_dict[node_name].continuous == True):
             restart_node(node_name)
         elif (node_dict[node_name].bond.is_broken() and node_dict[node_name].continuous == False):
-            #remove_node(node_name)
             items_to_remove.append(node_name)
     for node_name in items_to_remove:
         remove_node(node_name)
@@ -105,9 +140,14 @@ def check_nodes_still_running():
         del skill_dict[skill_name]
 
 
+# Calls rosrun on a node, if the node is to be restarted
 def restart_node(node_name):
+#TODO Test functionality of restart nodes on remote machines
     node_dict[node_name].bond.shutdown()
-    command = "rosrun {0} {1}".format(node_dict[node_name].node_package, node_dict[node_name].node_executable)
+    if (node_dict[node_name].node_machine == "local" or node_dict[node_name].node_machine == "localhost"):
+        command = "rosrun {0} {1}".format(node_dict[node_name].node_package, node_dict[node_name].node_executable)
+    else:
+        command = "ssh {0} 'rosrun {1} {2}'".format(node_dict[node_name].node_machine, node_dict[node_name].node_package, node_dict[node_name].node_executable)
     p = subprocess.Popen(command, shell=True)
     state = p.poll()
     if state is None:
@@ -121,34 +161,62 @@ def restart_node(node_name):
     node_dict[node_name].bond = bond
 
 
+# Removes node from skill machine.
 def remove_node(node_name):
     node_dict[node_name].bond.shutdown()
-    #TODO Add getters and setters to Skill class for nodes, to make code more readable
+#TODO Add getters and setters to Skill class for nodes, to make code more readable
     for skill_name in skill_dict:
         if (node_name in skill_dict[skill_name].node_name_list):
             skill_dict[skill_name].node_name_list.remove(node_name)
     del node_dict[node_name]
 
-#def handle_terminate_skill(request):
-#    terminate_skill(request.package, request.executable, request.node_name)
+
+#TODO Test
+def handle_terminate_skill(request):
+    terminate_skill(request.skill_name)
+    return 1
 
 
-#def terminate_skill(package, executable, node_name):
-#    for i in range(len(skill_list)):
-#        if (node_name == skill_list[i].node_name):
-#            skill_list[i].process.stop()
-#            bond_list[i].bond.shutdown()
-#            del skill_list[i]
+def terminate_skill(skill_name):
+    del_node_list = []
+    for node_name in node_dict:
+        if (skill_name in node_dict[node_name].skill_name_list):
+            del_node_list.append(node_name)
+    for node_name in del_node_list:
+        node_dict[node_name].skill_name_list.remove(skill_name)
+        if node_dict[node_name].skill_name_list == []:
+            node_dict[node_name].bond.break_bond()
+            node_dict[node_name].bond.shutdown()
+            del node_dict[node_name]
+#TODO Make this more accepting of other node formats
+            command = "rosnode kill {0}".format("/" + node_name)
+            p = subprocess.Popen(command, shell=True)
+            state = p.poll()
+            #if state is None:
+            #    pass
+            #elif state < 0:
+            #if state < 0:
+            #    rospy.loginfo("Process terminated with error")
+            #    return False
+            #elif state > 0:
+            #    pass
+    del skill_dict[skill_name]
+    return True
 
 
 def main():
+    # Object to store all skills launched by skill machine
     global skill_dict
     skill_dict = {}
+    #Object to store all nodes launched by skill machine
     global node_dict
     node_dict = {}
+#TODO Boot up FPGAs and face, etc.
     rospy.init_node('roboy_skill_machine')
-    s = rospy.Service('start_skill', StartSkill, handle_start_skill)
-    #t = rospy.Service('terminate_skill', TerminateSkill, handle_terminate_skill)
+    s = rospy.Service('launch_skill', LaunchSkill, handle_launch_skill)
+    t = rospy.Service('terminate_skill', TerminateSkill, handle_terminate_skill)
+    e = rospy.Service('execute_skill', ExecuteSkill, handle_execute_skill)
+    # Runs the check nodes method every 10 seconds
     while not rospy.is_shutdown():
         rospy.sleep(10.)
         check_nodes_still_running()
--- scripts/skill_machine_client.py
+++ scripts/skill_machine_client.py
@@ -4,10 +4,12 @@ import sys
 import rospy
 import distutils.util
 from ast import literal_eval
-import roboy_cognition_msgs.msg
+#import roboy_cognition_msgs.msg
 from roboy_skill_machine.msg import SkillNode
-from roboy_skill_machine.srv import StartSkill
-#from roboy_communication_cognition.msg import *
+from roboy_skill_machine.srv import LaunchSkill
+from roboy_skill_machine.srv import TerminateSkill
+from roboy_skill_machine.srv import ExecuteSkill
+from roboy_communication_cognition.msg import *
 
 #def start_skill_client(package, executable, node_name, continuous):
 #    pub = rospy.Publisher("startSkill", StartSkillMsg)
@@ -20,37 +22,66 @@ from roboy_skill_machine.srv import StartSkill
 #    pub.publish(msg)
     
 
-def start_skill_client(skill_name, launch_package, launch_file, continuous, node_tuple):
-    rospy.wait_for_service('start_skill')
+def launch_skill_client(skill_name, launch_package, launch_file, continuous, node_tuple):
+    rospy.wait_for_service('launch_skill')
     try:
         node_list = []
         for node in node_tuple:
-            #node_msg = SkillNode()
-            #node_msg.node_name = node[0]
-            #node_msg.node_executable = node[1]
-            #node_msg.node_package = node[2]
-
-            #node_msg = SkillNode(node[0], node[1], node[2])
-
-            node_msg = SkillNode(node_name=node[0], node_executable=node[1], node_package=node[2])
+            node_msg = SkillNode(node_name=node[0], node_executable=node[1], node_package=node[2], node_machine=node[3])
             node_list.append(node_msg)
-        start_skill = rospy.ServiceProxy('start_skill', StartSkill)
-        resp1 = start_skill(skill_name, launch_package, launch_file, continuous, node_list)
+        launch_skill = rospy.ServiceProxy('launch_skill', LaunchSkill)
+        resp1 = launch_skill(skill_name, launch_package, launch_file, continuous, node_list)
+        return True
+    except rospy.ServiceException, e:
+        print("Service call failed: %s"%e)
+
+def terminate_skill_client(skill_name):
+    rospy.wait_for_service('terminate_skill')
+    try:
+        terminate_skill = rospy.ServiceProxy('terminate_skill', TerminateSkill)
+        resp1 = terminate_skill(skill_name)
+        return True
+    except rospy.ServiceException, e:
+        print("Service call failed: %s"%e)
+
+def execute_skill_client(skill_name, command, continuous, node_tuple):
+    rospy.wait_for_service('execute_skill')
+    try:
+        node_list = []
+        #print(node_tuple)
+        #print(node_tuple[0])
+        #print(node_tuple[0][0])
+        for node in node_tuple:
+            node_msg = SkillNode(node_name=node[0], node_executable=node[1], node_package=node[2], node_machine=node[3])
+            node_list.append(node_msg)
+        execute_skill = rospy.ServiceProxy('execute_skill', ExecuteSkill)
+        resp1 = execute_skill(skill_name, command, continuous, node_list)
         return True
     except rospy.ServiceException, e:
         print("Service call failed: %s"%e)
 
 def usage():
-    return "%s [skill_name, launch_package, launch_file, continuous, node_list]"%sys.argv[0]
+    #return "%s [skill_name, launch_package, launch_file, continuous, node_list]"%sys.argv[0]
+    return "I'll get back to this. In the meantime, you're bad and you should feel bad."
 
 if __name__ == "__main__":
-    if len(sys.argv) == 6:
-        skill_name = str(sys.argv[1])
-        launch_package = str(sys.argv[2])
-        launch_file = str(sys.argv[3])
+    if (len(sys.argv) == 7 and str(sys.argv[1]) == "launch"):
+        #command = str(sys.argv[1])
+        skill_name = str(sys.argv[2])
+        launch_package = str(sys.argv[3])
+        launch_file = str(sys.argv[4])
+        continuous = distutils.util.strtobool(sys.argv[5])
+        node_tuple = literal_eval(sys.argv[6])
+        launch_skill_client(skill_name, launch_package, launch_file, continuous, node_tuple)
+    elif (len(sys.argv) == 3 and str(sys.argv[1]) == "terminate"):
+        skill_name = str(sys.argv[2])
+        terminate_skill_client(skill_name)
+    elif (len(sys.argv) == 6 and str(sys.argv[1]) == "execute"):
+        skill_name = str(sys.argv[2])
+        command = str(sys.argv[3])
         continuous = distutils.util.strtobool(sys.argv[4])
         node_tuple = literal_eval(sys.argv[5])
-    else:
+        execute_skill_client(skill_name, command, continuous, node_tuple)
+    else:  
         print usage()
         sys.exit(1)
-    start_skill_client(skill_name, launch_package, launch_file, continuous, node_tuple)

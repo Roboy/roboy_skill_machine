d774cedc05e6acd7a6afa0c23b12240d416b823b
missxa <aliona.kharchenko@gmail.com>
2019-03-20 19:13:41 +0100

    2nd fpga

--- launch/roboy_plexus.sh
+++ launch/roboy_plexus.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-ssh root@192.168.0.222 -t "bash -ic ./ROS_MASTER_URI_receiver_armhf && source ~/.bashrc && ./roboy_plexus" > 222.log &
+ssh root@192.168.0.222 -t "bash -ic ./ROS_MASTER_URI_receiver_armhf && source ~/.bashrc && ./roboy_plexus" > shoulder_right.log &
+ssh root@192.168.0.223 -t "bash -ic ./ROS_MASTER_URI_receiver_armhf && source ~/.bashrc && ./roboy_plexus" > shoulder_left.log &
 #ssh root@192.168.0.223 "export ROS_MASTER_URI='http://192.168.0.226:11311'; source /opt/ros/kinetic/setup.bash; ~/roboy_plexus" &

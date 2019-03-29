df0ea2e570ec1ffb709840de1da696daab9e6f61
missxa <aliona.kharchenko@gmail.com>
2019-03-29 16:42:55 +0100

    plexus

--- launch/roboy_plexus.sh
+++ launch/roboy_plexus.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-ssh root@192.168.0.222 -t "bash -ic ./ROS_MASTER_URI_receiver_armhf && source ~/.bashrc && ./roboy_plexus" > shoulder_right.log &
-ssh root@192.168.0.223 -t "bash -ic ./ROS_MASTER_URI_receiver_armhf && source ~/.bashrc && ./roboy_plexus" > shoulder_left.log &
+ssh root@192.168.0.220 -t "bash -ic 'source ~/.bashrc && ./roboy_plexus'" > /tmp/shoulder_right.log &
+#ssh root@192.168.0.223 -t "bash -ic ./ROS_MASTER_URI_receiver_armhf && source ~/.bashrc && ./roboy_plexus" > shoulder_left.log &
 #ssh root@192.168.0.223 "export ROS_MASTER_URI='http://192.168.0.226:11311'; source /opt/ros/kinetic/setup.bash; ~/roboy_plexus" &
